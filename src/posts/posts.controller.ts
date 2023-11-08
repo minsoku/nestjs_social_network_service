a7f1a9000b8c30801633d16ffeec1e81888e2d46
Seo MinSeok <minsoku03@inha.edu>
2023-11-08 11:33:41 +0900

    fix: make simple get method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,17 +10,59 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'minsoku1',
+    title: 'minsoku title1',
+    content: 'minsoku content1',
+    likeCount: 111,
+    commentCount: 111,
+  },
+  {
+    id: 2,
+    author: 'minsoku2',
+    title: 'minsoku title2',
+    content: 'minsoku content2',
+    likeCount: 222,
+    commentCount: 222,
+  },
+  {
+    id: 3,
+    author: 'minsoku3',
+    title: 'minsoku title3',
+    content: 'minsoku content3',
+    likeCount: 333,
+    commentCount: 333,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+  // 1) GET /posts
+  //    모든 post를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'author',
-      title: 'title',
-      content: 'content',
-      likeCount: 11,
-      commentCount: 22,
-    };
+  getPosts() {
+    return posts;
   }
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  //    ex) id=1일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+  // 3) POST /posts
+  //    POST를 생서한다.
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
 }
