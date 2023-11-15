acc8a4039cecf97a6df8bb322585a6a63c93d274
Seo MinSeok <minsoku03@inha.edu>
2023-11-15 17:49:30 +0900

    fix: relate posts.entity author to user.entity posts[]

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,12 +1,17 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class PostsModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  author: string;
+  // 1) UsersModel과 연동 Foreign key를 이용
+  // 2) null이 될 수 없음
+  @ManyToOne(() => UsersModel, (user) => user.posts, {
+    nullable: false,
+  })
+  author: UsersModel;
 
   @Column()
   title: string;
