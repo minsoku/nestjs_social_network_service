4af406818d6856a6cbde476de451f556da757b1e
Seo MinSeok <minsoku03@inha.edu>
2023-11-22 11:55:13 +0900

    fix: add registerWithEmail, hash

--- src/auth/const/auth.const.ts
+++ src/auth/const/auth.const.ts
@@ -1,1 +1,2 @@
 export const JWT_SECRET = 'minsoku';
+export const HASH_ROUNDS = 10;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
@@ -9,14 +9,36 @@ export class UsersService {
     @InjectRepository(UsersModel)
     private readonly usersRepository: Repository<UsersModel>,
   ) {}
-  async createSecureServer(nickname: string, email: string, password: string) {
-    const user = this.usersRepository.create({
-      nickname,
-      email,
-      password,
+  async createUser(user: Pick<UsersModel, 'nickname' | 'email' | 'password'>) {
+    // 1) nickname 중복이 없는 지 확인
+    // exist() -> 만약에 조건에 해당되는 값이 있으면 true 반환
+    const nicknameExists = await this.usersRepository.exist({
+      where: {
+        nickname: user.nickname,
+      },
     });
 
-    const newUser = await this.usersRepository.save(user);
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다.');
+    }
+
+    const emailExist = await this.usersRepository.exist({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (emailExist) {
+      throw new BadRequestException('이미 존재하는 email 입니다.');
+    }
+
+    const userObject = this.usersRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.usersRepository.save(userObject);
 
     return newUser;
   }
