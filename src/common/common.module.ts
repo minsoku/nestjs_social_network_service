c406f7eada07962737ebef6d93284b3db53f93e4
Seo MinSeok <minsoku03@inha.edu>
2023-12-07 22:54:12 +0900

    fix: 파일 업로드 기능 common 으로 이동

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,50 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+import { v4 as uuid } from 'uuid';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { TEMP_FOLDER_PATH } from './const/path.const';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    MulterModule.register({
+      limits: {
+        // byte 단위
+        fileSize: 100000000,
+      },
+      fileFilter: (req, file, cb) => {
+        /**
+         * cb(에러, boolean)
+         *
+         * 첫 번째 파라미터에는 에러가 있을 경우 에러 정보를 넣어준다.
+         * 두 번째 파라미터는 파일을 받을지 말지 boolean을 넣어준다.
+         */
+        // xxx.jpg -> .jpg
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png파일만 업로드 가능합니다!'),
+            false,
+          );
+        }
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  ],
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
